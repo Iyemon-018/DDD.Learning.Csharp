b589926cb8123e9f36b387f541a28052d41e2cde
iyemon018 <iyemon018@gmail.com>
2021-01-11 23:04:10 +0900

    UI とインフラストラクチャー 手前まで

--- Tests/DDD.Tests/WinForm/MeasureViewModelTest.cs
+++ Tests/DDD.Tests/WinForm/MeasureViewModelTest.cs
@@ -2,7 +2,8 @@
 {
     using ChainingAssertion;
     using DDD.WinForm.ViewModels;
-    using Mock.Infrastructure;
+    using Domain.Repositories;
+    using Moq;
     using Xunit;
     
     public class MeasureViewModelTest
@@ -10,9 +11,17 @@
         [Fact]
         public void Test_計測_シナリオ()
         {
-            var viewModel = new MeasureViewModel(new SensorMock());
+            var sensorMock = new Mock<ISensorRepository>();
+            sensorMock.Setup(x => x.GetData()).Returns(1.23456f);
+
+            var viewModel  = new MeasureViewModel(sensorMock.Object);
+
             viewModel.Measure();
             viewModel.MeasureValue.Is("1.23 m/s");
+
+            sensorMock.Setup(x => x.GetData()).Returns(2.2f);
+            viewModel.Measure();
+            viewModel.MeasureValue.Is("2.2 m/s");
         }
     }
 }
